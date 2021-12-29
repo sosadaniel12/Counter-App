f2caa88ab09010ca306fa801b414105e85bf01ff
Daniel Sosa <danielsosa@Daniels-MacBook-Air.local>
2021-12-28 21:32:46 -0600

    working on counter

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,6 +1,10 @@
 import React from "react"
+import Counter from "./Counter"
+
 function App() {
-  return null
+  return (
+    <Counter intialCount={0}/>
+  )
 }
 
 export default App;
---
+++ counter-app/src/Counter.js
@@ -0,0 +1,12 @@
+import React , { Component} from "react"
+
+export default class Counter extends Component {
+    render() {
+        return
+        <div>
+        <button>-</button>
+        <span>0</span>
+        <button>+</button>
+      </div>
+    }
+}
